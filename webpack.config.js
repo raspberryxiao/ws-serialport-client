33d54192fc674828402a7282e8f702cef7b6bc75
Alex Tan <tanjiaozhong@codemao.cn>
2018-11-21 15:53:53 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const pkg = require('./package.json');
+console.log(pkg.name);
+
+module.exports = function webpackConfig (env = {}) {
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      index: './index.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'ws-serialport-client.min.js',
+      library: 'ws-serialport-client',
+      libraryTarget: 'umd',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          // exclude: /(node_modules|bower_components)/,
+          use: [{
+            loader: 'babel-loader',
+          }]
+        },
+        {
+          'test': /\.tsx?$/,
+          use: [{
+            loader: 'ts-loader',
+          }],
+          'exclude': [/node_modules/, nodeModulesPath]
+        },
+      ]
+    },
+  };
+};
